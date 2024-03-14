2754383a52524827bdbe8beefe173115faa3148d
W r e n c h <134978254+Jeanpseven@users.noreply.github.com>
2024-03-14 20:08:09 -0300

    Create vizinhos2.py

---
+++ vizinhos2.py
@@ -0,0 +1,58 @@
+import requests
+import time
+import re
+import os
+import platform
+
+def clear():
+    print("--------=DEDSEC=Wrench=--------")
+
+R='\033[1;31m'
+B='\033[1;34m'
+C='\033[1;37m'
+Y='\033[1;33m'
+G='\033[1;32m'
+
+code_info = C + '[' + Y + 'i' + C + '] '
+code_details = C + '[' + G + '*' + C + '] '
+code_result = C + '[' + G + '+' + C + '] '
+code_error = C + '[' + R + '!' + C + '] '
+
+def main():
+    clear()
+    print("\n" + code_info + "Vizinhos.")
+    print(f'''
+    {C}[{G}i{C}] Formas de operação:
+    [{G}1{C}] Consultar CPF.
+    [{G}2{C}] Voltar.
+    [{G}3{C}] {R}Sair.{C}
+    ''')
+    tool=input(f'{C}[{G}+{C}] Selecione a forma de operação:{B} ')
+    if tool=='1':
+        cpf=input(f'{C}[{G}*{C}] Informe o CPF a ser consultado (sem >')
+        filtrar_por_nome(cpf)
+    elif tool=='2':
+        clear()
+        import consulta
+        consulta.main()
+    elif tool=='3':
+        clear()
+        print(f'\n{G}Somos uma legião.{C}\n')
+        exit()
+    else:
+        clear()
+        print(f'{C}[{R}-{C}] Seleção inválida.')
+        time.sleep(0.2)
+        main()
+
+def filtrar_por_nome(cpf):
+    nomes = requests.get(f"https://tudosobretodos.info/{cpf}").text
+    viz = re.findall(r"[A-Z][a-z]+ [A-Z][a-z]+", nomes)
+    if viz:
+        print(f'\n{code_result} Vizinhos encontrados:')
+        for vizinho in viz:
+            print(f'{code_result} - {vizinho}')
+    else:
+        print(f'{code_error} Nenhum vizinho encontrado para o CPF "{cpf}".')
+
+main()
