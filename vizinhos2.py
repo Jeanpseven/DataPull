38cda6db4f9be049fc5630fcbeea90dbfdcbe109
W r e n c h <134978254+Jeanpseven@users.noreply.github.com>
2024-03-14 20:38:03 -0300

    Update vizinhos2.py

--- vizinhos2.py
+++ vizinhos2.py
@@ -5,6 +5,12 @@ import os
 import platform
 
 def clear():
+    if platform.system() == "Windows":
+        os.system("cls")
+    elif platform.system() == "Linux":
+        os.system("clear")
+    else:
+        os.system("clear")
     print("--------=DEDSEC=Wrench=--------")
 
 R='\033[1;31m'
@@ -27,16 +33,17 @@ def main():
     [{G}2{C}] Voltar.
     [{G}3{C}] {R}Sair.{C}
     ''')
-    tool=input(f'{C}[{G}+{C}] Selecione a forma de operação:{B} ')
-    if tool=='1':
-        cpf=input(f'{C}[{G}*{C}] Informe o CPF a ser consultado :')
+    tool = input(f'{C}[{G}+{C}] Selecione a forma de operação:{B} ')
+    if tool == '1':
+        cpf = input(f'{C}[{G}*{C}] Informe o CPF a ser consultado (sem espaço, sem ponto e sem traço):')
         cpf = cpf.replace(' ', '').replace('.', '').replace('-', '')  # Remover espaços, pontos e traços
-        filtrar_por_nome(cpf)
-    elif tool=='2':
+        nome_filtro = input(f'{C}[{G}*{C}] Informe o nome a ser filtrado:')
+        filtrar_por_nome(cpf, nome_filtro)
+    elif tool == '2':
         clear()
         import consulta
         consulta.main()
-    elif tool=='3':
+    elif tool == '3':
         clear()
         print(f'\n{G}Somos uma legião.{C}\n')
         exit()
@@ -46,14 +53,15 @@ def main():
         time.sleep(0.2)
         main()
 
-def filtrar_por_nome(cpf):
+def filtrar_por_nome(cpf, nome_filtro):
     nomes = requests.get(f"https://tudosobretodos.info/{cpf}").text
     viz = re.findall(r"[A-Z][a-z]+ [A-Z][a-z]+", nomes)
-    if viz:
+    filtered_viz = [v for v in viz if nome_filtro.lower() in v.lower()]
+    if filtered_viz:
         print(f'\n{code_result} Vizinhos encontrados:')
-        for vizinho in viz:
+        for vizinho in filtered_viz:
             print(f'{code_result} - {vizinho}')
     else:
-        print(f'{code_error} Nenhum vizinho encontrado para o CPF "{cpf}".')
+        print(f'{code_error} Nenhum vizinho encontrado com o nome "{nome_filtro}".')
 
 main()
